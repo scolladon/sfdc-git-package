50521ddbc3ba1e8b78122f4f819fb3ab00d1ceb2
Sébastien Colladon <scolladon@salesforce.com>
2016-07-17 23:08:31 +0200

    Added default from + cleaning

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function(orchestrator,program,pjson){
 		.version(pjson.version)
 		.option('-b, --branch [name]', 'branch on which the diff is done [master]', 'master')
 		.option('-t, --to [sha]', 'commit sha from where the diff is done [HEAD]', 'HEAD')
-		.option('-f, --from [sha]', 'commit sha from where the diff is done [git rev-list --max-parents=0 HEAD]', '`git rev-list --max-parents=0 HEAD`')
+		.option('-f, --from [sha]', 'commit sha from where the diff is done [git rev-list --max-parents=0 HEAD]', '')
 		.option('-o, --output [dir]', 'package.xml specific output [./output]', './output')
 		.option('-a, --api-version [version]', 'salesforce API version [37.0]', '37.0')
 		.option('-r, --repo [dir]', 'git repository location [./repo]', './repo')
