a6ba489c4a4c17cf025d952c15505075e0fa83df
scolladon <colladonsebastien@gmail.com>
2019-07-05 19:48:40 +0200

    use strict, classes and arrow functions

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
-const gitDiff = require('./lib/gitDiffHandler.js');
-const pack = require('./lib/packageConstructor');
-const fileUtils = require('./lib/utils/fileUtils');
+'use strict';
+const GitDiffHandler = require('./lib/gitDiffHandler.js');
+const PackageConstructor = require('./lib/packageConstructor');
+const FileUtils = require('./lib/utils/fileUtils');
 
-module.exports = function(config, logger) {
+module.exports = (config) => {
 
-  return new Promise(function (resolve, reject) {
+  return new Promise((resolve, reject) => {
 
     if(typeof config.to === 'undefined' || config.to === null
     || typeof config.from === 'undefined' || config.from === null
     || typeof config.apiVersion === 'undefined' || config.apiVersion === null
     || typeof config.output === 'undefined' || config.output === null
     || typeof config.repo === 'undefined' || config.repo === null) {
-      logger('Not enough config options');
-      return;
+      return reject('Not enough config options');
     }
 
-    const git = new gitDiff(config);
-    const pc = new pack(config);
-    const fu = new fileUtils(config);
+    const git = new GitDiffHandler(config);
+    const pc = new PackageConstructor(config);
+    const fu = new FileUtils(config);
 
     git.diff()
-    .then(pc.constructPackage)
-    .then(fu.writeAsync)
-    .then(function(res){
-      if(res){
-        res.forEach(function(name){
-          logger(name + ' created');
-        });
-      }
-      resolve();
+    .then(structuredDiffs=>pc.constructPackage(structuredDiffs))
+    .then(filesContent=>fu.writeAsync(filesContent))
+    .then(res => {
+      resolve(res);
     })
-    .catch(function(err){reject(err)})
+    .catch(err=>reject(err))
   });
 };
